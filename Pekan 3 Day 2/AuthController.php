55af3b3e2577bd13ca9d84378f2125ce57d2bc53
Fieradhila <49020565+Fieradhila@users.noreply.github.com>
2020-08-05 21:48:45 +0700

    Laravel Web Statis

---
+++ Pekan 3 Day 2/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //Route Register dan Welcome
+    public function register(){
+        return view('register');
+    }
+
+    /*public function welcome_get(Request $request){
+        //dd($request["lastName"]);
+        return view('masuk');
+    }*/
+
+    public function welcome(Request $request){
+        //dd($request->all());
+        $firstName = $request["firstName"];
+        $lastName = $request["lastName"];
+        return view('masuk')
+            ->with('firstName', $firstName)
+            ->with('lastName', $lastName);
+    }
+}
